6bd6f81f87530c048412f48e937fa70fd8115fa6
martin <a-msigne@expedia.com>
2015-12-28 13:07:00 -0500

    Code refactoring

--- src/main/java/com/camlait/global/erp/produit/GlobalErpProduitApplication.java
+++ src/main/java/com/camlait/global/erp/produit/GlobalErpProduitApplication.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.camlait.global.erp.domain.config.GlobalAppConstants;
