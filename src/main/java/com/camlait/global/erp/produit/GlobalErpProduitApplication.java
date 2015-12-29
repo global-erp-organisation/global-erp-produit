d7e0dca3ba0818962c15ec4f42ee1c9052f4953b
martin <a-msigne@expedia.com>
2015-12-29 14:29:47 -0500

    code refactoring

--- src/main/java/com/camlait/global/erp/produit/GlobalErpProduitApplication.java
+++ src/main/java/com/camlait/global/erp/produit/GlobalErpProduitApplication.java
@@ -12,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.camlait.global.erp.domain.config.GlobalAppConstants;
+import com.camlait.global.erp.domain.produit.CategorieProduit;
 import com.camlait.global.erp.domain.produit.Produit;
 import com.camlait.global.erp.service.produit.IProduitService;
 
@@ -20,22 +21,27 @@ import com.camlait.global.erp.service.produit.IProduitService;
 @EntityScan(GlobalAppConstants.DOMAIN_BASE_PACKAGE)
 @EnableJpaRepositories(GlobalAppConstants.DAO_BASE_PACKAGE)
 public class GlobalErpProduitApplication {
-
-	@Autowired
-	IProduitService produitService;
-
-	public static void main(String[] args) {
-		SpringApplication.run(GlobalErpProduitApplication.class, args);
-	}
-
-	@PostConstruct
-	public void test() {
-
-		Collection<Produit> produits = produitService.listerProduit(1L);
-		System.out.println("------------------------------------------");
-		produits.stream().forEach(pr -> {
-			System.out.println(pr);
-		});
-		System.out.println("-------------------------------------------");
-	}
+    
+    @Autowired
+    IProduitService produitService;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalErpProduitApplication.class, args);
+    }
+    
+    @PostConstruct
+    public void test() {
+        System.out.println("------------------------------------------");
+        try {
+            CategorieProduit c = produitService.obtenirCategorieProduit(3L);           
+            Collection<Produit> produits = produitService.listerProduit(c);
+            produits.stream().forEach(pr -> {
+                System.out.println(pr);
+            });
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("-------------------------------------------");
+    }
 }
